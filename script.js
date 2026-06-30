ab206ec49dcc890779dcd1f7fdd0f96b6f08602e
imani <iyrojas@asu.edu>
2026-06-30 15:15:29 -0400

    created js
    adding in recommendation songs

---
+++ script.js
@@ -0,0 +1,7 @@
+const recommendationSection = document.getElementById("recommendation-section");
+const recommendationTitle = document.getElementById("recommendation-title");
+const songList = document.getElementById("song-list");
+
+const moodSongs = {
+    
+}
